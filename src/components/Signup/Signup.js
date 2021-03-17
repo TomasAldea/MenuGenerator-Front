956380a4efdb207f27a68fe91da424cad2703883
TomasAldea <tomas.sbd.5@gmail.com>
2021-03-17 13:38:42 +0100

    redirect a views con mensajes para el usuario

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -5,7 +5,11 @@ import { Redirect } from "react-router-dom";
 export function Signup() {
   const auth = React.useContext(AuthContext);
 
-  const [state, setState] = React.useState({ email: "", password: "" });
+  const [state, setState] = React.useState({
+    email: "",
+    password: "",
+    name: "",
+  });
   const [redirect, setRedirect] = React.useState(false);
 
   const handleChange = (event) => {
@@ -15,7 +19,7 @@ export function Signup() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-   await auth.handleSignup(state);
+    await auth.handleSignup(state);
     setRedirect(true);
   };
   if (redirect) {
@@ -32,6 +36,15 @@ export function Signup() {
         margin: "10px auto",
       }}
     >
+      <label htmlFor="email">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={state.name}
+        onChange={handleChange}
+        required
+      />
       <label htmlFor="email">Email</label>
       <input
         type="text"
