eb2fe87a77804ecdb43b3947f0aa54b6e657a77f
TomasAldea <tomas.sbd.5@gmail.com>
2021-03-27 15:05:44 +0100

    error 404

--- src/Views/RecipeEdit/RecipeEdit.js
+++ src/Views/RecipeEdit/RecipeEdit.js
@@ -5,23 +5,29 @@ import { editRecipe } from "../../service/recipe.service";
 import { recipe as recipeService } from "../../service/recipe.service";
 import "./RecipeEdit.css";
 import { useParams } from "react-router-dom";
+import {uploadFileService} from "../../service/recipe.service"
 
 const initialStateEdit = {
   name: "",
   ingredients: "",
   description: "",
   category: "",
+  picture: "",
 };
 
+
 export function RecipeEdit() {
-    
-
-  //----- get recipe to placeholder imputs -----//
-
 
   const [state, setState] = React.useState(initialStateEdit);
 
   const { recipeId } = useParams();
+
+  const handleUpload = async (e) => {
+    const uploadData = new FormData();
+    uploadData.append("image", e.target.files[0]);
+    const { data } = await uploadFileService(uploadData);
+    setState({ ...state, image: data });
+  };
 
   const getRecipe = async (id) => {
     const { data } = await recipeService(id);
@@ -30,9 +36,8 @@ export function RecipeEdit() {
       ingredients: data.ingredients,
       description: data.description,
       category: data.category,
-    })
+    });
   };
-
 
   React.useEffect(() => {
     getRecipe(recipeId);
@@ -46,17 +51,14 @@ export function RecipeEdit() {
     { value: "desert", label: "desert" },
   ];
 
- //----- form states and handlefunctions -----//
-
+  //----- form states and handlefunctions -----//
 
   const [redirect, setRedirect] = React.useState(false);
-
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setState({ ...state, [name]: value });
   };
-
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -69,42 +71,51 @@ export function RecipeEdit() {
 
   return (
     <div className="padding-container">
-    <form className="create-recipe" onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        value={state.name}
-        onChange={handleChange}
-      />
-      <label htmlFor="ingredients">Ingredients</label>
-      <input
-        type="text"
-        name="ingredients"
-        id="ingredients"
-        value={state.ingredients}
-        onChange={handleChange}
-      />
-      <label htmlFor="category">Category</label>
-      <Select
-        options={options}
-        onChange={({ value }) => setState({ ...state, category: value })}
-      />
+      <form className="create-recipe" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={state.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="ingredients">Ingredients</label>
+        <input
+          type="text"
+          name="ingredients"
+          id="ingredients"
+          value={state.ingredients}
+          onChange={handleChange}
+        />
+        <label htmlFor="category">Category</label>
+        <Select
+          options={options}
+          onChange={({ value }) => setState({ ...state, category: value })}
+        />
 
-      <label htmlFor="description">Description</label>
-      <textarea
-        name="description"
-        id="description"
-        cols="30"
-        rows="10"
-        value={state.description}
-        onChange={handleChange}
-      />
-      <button type="submit" className="btn btn-light">
-      <img src="/img/checked.png"  alt="checkIcon"></img>
-      </button>
-    </form>
+        <label htmlFor="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+          value={state.description}
+          onChange={handleChange}
+        />
+        <label htmlFor="file">file</label>
+
+        <input
+          type="file"
+          name="picture"
+          id="picture"
+          value={state.picture}
+          onChange={handleUpload}
+        />
+        <button type="submit" className="btn btn-light">
+          <img src="/img/checked.png" alt="checkIcon"></img>
+        </button>
+      </form>
     </div>
   );
 }
